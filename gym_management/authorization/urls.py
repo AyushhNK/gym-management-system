ff5e4eca9d5a3a817e72cf75f510049bc49bf014
VINANK <neupaneayush3@gmail.com>
2024-07-05 18:04:44 +0545

    authorization

--- gym_management/authorization/urls.py
+++ gym_management/authorization/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import SignupView,LoginView
+
+router=DefaultRouter()
+
 
 urlpatterns=[
-	path('home/',views.home,name="home")
+	path('signup',SignupView.as_view(),name='signup'),
+	path('login',LoginView.as_view(),name='login'),
 ]
--- gym_management/authorization/views.py
+++ gym_management/authorization/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.models import User
-
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 # Create your views here.
 
-def home(request):
-	users=User.objects.all()
-	return HttpResponse(users[0].is_staff)
+class SignupView(APIView):
+	def post(self,request):
+		username=request.data.get('username')
+		password=request.data.get('password')
+		user=User.objects.create(Username=username,Password=password)
+		user.save()
+		return Response({"message":"sucessfully create user"})
+
+class LoginView(APIView):
+	def post(self,request):
+		username=request.data.get('username')
+		user=User.objects.get(Username=username)
+		serializer=UserSerializer(user)
+		return Response(serializer.data)
+
 
 
